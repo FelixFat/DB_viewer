5f64df6f5e0a6320754e66ee2815aec46d0b14f2
Daniil Fataliev <felixfat9@gmail.com>
2021-12-04 14:59:42 +0300

    upd

--- src/main.py
+++ src/main.py
@@ -2,31 +2,27 @@ import os
 import sys
 import sqlite3
 
-
 from PyQt5 import QtWidgets, uic
 
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('..//ui//UI_Form.ui', self)
+
+        self.button = self.findChild(QtWidgets.QPushButton, 'pushButton') # Find the button
+        self.button.clicked.connect(self.printButtonPressed) # Remember to pass the definition/method, not the return value!
+
+        self.show()
+
+    def printButtonPressed(self):
+        pass
 
 def main():
-    path_ui = os.getcwd()[:-4] + '\\ui\\UI_Form.ui'
-    path_db = os.getcwd()[:-4] + '\\Study_session.db'
-
-    app = QtWidgets.QApplication([])
-    win = uic.loadUi(path_ui)
-
-    ###
-    
-    db = sqlite3.connect(path_db)
-    cursor = db.cursor()
-    cursor.execute('SELECT * FROM Студенты')
-
-    table_db = cursor.fetchall()
-
-    ###
-
-    win.show()
-    sys.exit(app.exec())
-
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
     return
+
 
 if __name__ == '__main__':
     main()
